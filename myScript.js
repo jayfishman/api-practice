a41bce0f5d78c90a54d70ce23e6d9ce239598e13
Jay Fishman <chaim.fishman@nytimes.com>
2017-07-26 10:36:48 -0400

    ?

--- myScript.js
+++ myScript.js
@@ -1,9 +1,11 @@
 
+var app = {
 
-
-$('.submit').click(function() {
-	alert($('.CityInput.val()'))
-	}
-});
+	initialize: function(){
+		$('.submit').click(function() {
+			alert('fdsfdsdsf')
+			//alert($('.CityInput.val()'))
+		});
+};
 
     //API-ID 'APPID=b8fa89ce01d350743d89dc466f6ec135';
