b67be95d8f10b59c241923254a7208625ee52180
Jay Fishman <chaim.fishman@nytimes.com>
2017-07-26 15:03:14 -0400

    -

--- myScript.js
+++ myScript.js
@@ -4,10 +4,15 @@ var app = {
 		console.log('hello there');
 		$('#submit').click(function() {
 			console.log('haha')
-			//alert('submitted');
-			alert($('#CityInput).val()')
+			var city = $('#CityInput').val();
+			var myApiID = 'APPID=b8fa89ce01d350743d89dc466f6ec135';
+			var url = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&' + myApiID;
+
+			$.get('url', function(data){
+				console.log(data)
+			});
 		});
 	}
 }
 
-    //API-ID 'APPID=b8fa89ce01d350743d89dc466f6ec135';
+    
