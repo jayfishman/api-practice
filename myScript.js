0e7487caa304cb6b244f5391c4911ae6f23c087e
jayfishman <chaimfishman2@gmail.com>
2017-07-26 11:00:05 -0400

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,11 +2,12 @@
 var app = {
 
 	initialize: function(){
+		console.log('hello there');
 		$('#submit').click(function() {
-			alert('fdsfdsdsf')
-			//alert($('.CityInput.val()'))
+			alert('fdsfdsdsf');
+			//alert($('#CityInput.val()'))
 		});
 	}
-};
+}
 
     //API-ID 'APPID=b8fa89ce01d350743d89dc466f6ec135';
